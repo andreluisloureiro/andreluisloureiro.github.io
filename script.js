187715172e47afde9ebdaaebeb04c8d2f00c49a1
André Luis Loureiro <andreluisloureiro9@gmail.com>
2024-01-10 19:55:54 -0300

    Better javascript code
    More dynamic and overall better code for javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+let nav = document.getElementById("nav")
+
+function reveal() {
+    if (window.innerWidth > 678) {
+        if (window.scrollY > 30) {
+            nav.classList.add("header")
+        } else {
+            nav.classList.remove("header")
+        }
+    }else {
+        if (window.scrollY >= 10) {
+            nav.classList.add("header")
+        }
+        if (window.scrollY <= 5) {
+            nav.classList.remove("header")
+        }
+    }
+
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+if (location.hash != "") {
+    location.href = location.hash;
+}
